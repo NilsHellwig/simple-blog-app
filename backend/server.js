7c4d8b35e1cf8a13ded40cc0b88286437bbf26d4
NilsHellwig <nc.hellwig@gmail.com>
2025-07-11 18:30:43 +0200

    add code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import { postSchema, userSchema } from "./schema.js"
 
 const frontendPort = process.env.FRONTEND_PORT || 5173;
 
-const mongoUri = process.env.MONGODB_URI || "monogodb://localhost:27017/blogapp";
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/blogapp";
 const port = process.env.PORT || 3000;
 
 const User = mongoose.model("User", userSchema);
@@ -21,6 +21,28 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(cors({ origin: `http://localhost:${frontendPort}` }));
 app.use("/images", express.static("/mongo_img"));
+
+app.post("/register", async (req, res) => {
+    const { username, password, name } = req.body;
+
+    const hashedPassword = bcrypt.hash(password);
+
+    try {
+        const user = await User.create({ username, password: hashedPassword, name });
+        const token = jwt.sign({ username: user.username, name: user.name }, secretKey, { expiresIn: "3h" });
+
+        res.status(201).json({
+            token, user: {
+                username: user.username,
+                name: user.name
+            }
+        })
+    } catch (e) {
+        res.status(400).json({"error": "Username already taken or invalid password."});
+    }
+
+
+})
 
 mongoose.connect(mongoUri).then(() => {
     app.listen(port, () => {
